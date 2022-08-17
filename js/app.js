fbab89031cba9f0f5965c6cbe0a6c49bffd87dfb
nirsade1 <nir.sade@gmail.com>
2022-08-17 15:00:34 +0300

    nav1

---
+++ js/app.js
@@ -0,0 +1,14 @@
+
+let navToggle = document.querySelectorAll(".nav-toggle");
+let links = document.querySelector(".links");
+
+
+
+navToggle.addEventListener("click",function(){
+    links.classList.toggle("show-list")});
+
+
+
+
+    
+
