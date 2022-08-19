abbba28a7aeb61fc79e4014badcade7c9220c828
Rom Burla <romwebdev@gmail.com>
2022-08-19 03:16:58 +0300

    footer and modals and sponsers

--- app/app.js
+++ app/app.js
@@ -1,17 +1,48 @@
-import { sponsers } from "./sponsers.js";
+import { sponsers } from "./sponsors.js";
 let navToggle = document.querySelector(".nav-toggle");
 let links = document.querySelector(".links");
+let footer = document.querySelector("footer");
 let modalArr = [];
-let modalContainer = document.getElementById("modal-container");
-navToggle.addEventListener("click", function () {
+let modalContainer = document.createElement("div");
+let secondaryHeading = document.createElement("h3");
+let copyright = document.createElement("p");
+
+navToggle.addEventListener("click", () => {
   links.classList.toggle("show-links");
 });
-
-footer.innerHtml = `<p class="copyright">Copyright &copy; 2022 by Nir & Rom. All rights reserved.</p>`;
-footer.innerHtml = `<p class="copyright">Copyright &copy; 2022 by Nir & Rom. All rights reserved.</p>`;
-
+footer.appendChild(secondaryHeading);
+secondaryHeading.innerHTML = `Our Festival's Sponsers`;
+footer.appendChild(modalContainer);
+modalContainer.className = "modal-container";
 for (let i = 0; i < sponsers.length; i++) {
-  let modal = document.createElement("div");
+  let modal = document.createElement("button");
+  modal.id = i;
   modalArr.push(modal);
-  footer.appendChild(modalArr[i]);
+  modalContainer.appendChild(modalArr[i]);
+  console.log(modalArr);
+  modalArr[i].style.backgroundImage = `url("${sponsers[i].logo}")`;
+  modalArr[i].className = "modal";
+  let modalContent = document.createElement("div");
+  modalContent.className = "modal-content";
+  document.body.appendChild(modalContent);
+  let modalContentActive = document.createElement("div");
+  modalContentActive.className = "modal-content-active";
+  modalContent.appendChild(modalContentActive);
+  modalContentActive.innerHTML = `<img src = "${sponsers[i].logo}" alt = "${sponsers[i].name} logo">
+  <h1>${sponsers[i].name}</h1>
+  <img src = "${sponsers[i].image}" alt = "${sponsers[i].name}">
+  <p>${sponsers[i].description}</p>
+  <h3><a href="${sponsers[i].website}">${sponsers[i].website}</h3>`;
+  modalArr[i].addEventListener("click", function () {
+    modalContent.style.display = "block";
+  });
+  window.addEventListener("click", function (event) {
+    if (event.target == modalContent) {
+      modalContent.style.display = "none";
+    }
+  });
 }
+footer.appendChild(copyright);
+copyright.innerHTML += `<p class="copyright">
+Copyright &copy; 2022 by Nir & Rom. All rights reserved.
+</p>`;
