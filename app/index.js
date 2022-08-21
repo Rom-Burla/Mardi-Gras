2d70b3b916ea4fd2c1352f18f7594d9b91a22121
Rom Burla <romwebdev@gmail.com>
2022-08-21 22:11:09 +0300

    index paragraphs

--- app/index.js
+++ app/index.js
@@ -1,8 +1,8 @@
 "use strict";
 import { sliderPics } from "./sliderpics.js";
 
-let section = document.getElementsByTagName("section")[0];
-
+const section = document.getElementsByTagName("section")[0];
+const article = document.getElementsByTagName("article")[0];
 let slideContainer = document.createElement("div");
 slideContainer.className = "slide-container";
 section.appendChild(slideContainer);
@@ -77,3 +77,10 @@ section.appendChild(mobileImgContainer);
 mobileImgContainer.appendChild(mobileImg);
 mobileImgContainer.appendChild(mobileImgDescription);
 //end of mobile image
+
+let mardiExplain = document.createElement("div");
+article.appendChild(mardiExplain);
+mardiExplain.innerHTML = `<p>The holiday of Mardi Gras is celebrated in all of Louisiana, including the city of New Orleans. Celebrations are concentrated for about two weeks<br> before and through Shrove Tuesday, the day before Ash Wednesday (the start of lent in the Western Christian tradition). Usually there is one major <br>parade each day (weather permitting); many days have several large parades. The largest and most elaborate parades take place the last five days<br> of the Mardi Gras season. In the final week, many events occur throughout New Orleans and surrounding communities,<br> including parades and balls (some of them masquerade balls).</p>
+<p>The parades in New Orleans are organized by social clubs known as krewes; most follow the same parade schedule and route each year. The<br> earliest-established krewes were the Mistick Krewe of Comus, the earliest, Rex, the Knights of Momus and the Krewe of Proteus. Several modern<br> "super krewes" are well known for holding large parades and events (often featuring celebrity guests), such as the Krewe of Endymion, the<br> Krewe of Bacchus, as well as the Zulu Social Aid & Pleasure Club—a predominantly African American krewe. Float riders traditionally toss throws into<br> the crowds. The most common throws are strings of colorful plastic beads, doubloons, decorated plastic "throw cups", Moon Pies, and small<br> inexpensive toys. Major krewes follow the same parade schedule and route each year.</p>
+<p>While many tourists center their Carnival season activities on Bourbon Street, major parades originate in the Uptown and Mid-City districts and follow<br> a route along St. Charles Avenue and Canal Street, on the upriver side of the French Quarter. Walking parades - most notably the Krewe du Vieux and Chewbacchus - also take place downtown in the Faubourg Marigny and French Quarter in the weekends preceding Mardi Gras day. Mardi Gras day traditionally concludes with the "Meeting of the Courts" between Rex and Comus.</p>`;
+mardiExplain.className = "mardi-explain";
